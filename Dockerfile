a946ed25df808ead98912b2a2930242717a1ac21
Angelo Dini <angelo.dini@divio.ch>
2021-12-17 11:02:19 +0100

    bumped to version 1.3.0.0

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,12 @@
 # </DOCKER_FROM>
 
 # <NODE>
+ENV NVM_DIR=/opt/nvm \
+    NVM_VERSION=0.39.0
+ENV NODE_VERSION=16.13.1 \
+    NPM_VERSION=8.3.0
+
 ADD build /stack/boilerplate
-
-ENV NODE_VERSION=14.17.1 \
-    NPM_VERSION=6.14.13
-
 RUN bash /stack/boilerplate/install.sh
 
 ENV NODE_PATH=$NVM_DIR/versions/node/v$NODE_VERSION/lib/node_modules \
--- build/install.sh
+++ build/install.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-SCRIPT=$(readlink -f "$0")
-BASEDIR=$(dirname "$SCRIPT")
+mkdir -p $NVM_DIR
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v$NVM_VERSION/install.sh | bash
 
-bash ${BASEDIR}/node.sh
+source $NVM_DIR/nvm.sh
+nvm install $NODE_VERSION
+nvm alias default $NODE_VERSION
+nvm use default
+
+npm install -g npm@"$NPM_VERSION"
+npm install -g gulp bower
